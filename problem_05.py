16590c79fcd8c531931731d5721f82fdcafedf91
apu <40669057+xrayapu@users.noreply.github.com>
2024-07-30 22:48:41 +0600

    Create problem_05.py

---
+++ problem_05.py
@@ -0,0 +1,26 @@
+# problem 5: largest subarray sum of k.
+# 4,1,1,1,2,3,5 ->4,1 ->1,1,1,2 ->2,3, -> 5
+#  ans: 4(1,1,1,2 array's length)
+
+def sol(arr,k):
+    i=j=0
+    mx=0
+    ans=0
+
+    while j< len(arr):
+        ans+=arr[j]
+        if ans < k:
+            j+=1
+        elif ans==k:
+            mx=max(mx,j-i+1)
+            j+=1
+        else: 
+            while ans > k:
+                ans-= arr[i]
+                i+=1
+                
+            j+=1
+        
+    return mx
+
+print(sol([4,1,1,2,3,5],8))
