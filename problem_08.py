9480f7ccdcfafade47810da848ee0943b1c136af
apu <40669057+xrayapu@users.noreply.github.com>
2024-07-31 00:37:40 +0600

    Create problem_08.py

---
+++ problem_08.py
@@ -0,0 +1,33 @@
+# problem -08:pick up toys. same problem as 06 but k=2 here just ! 
+
+def sol(arr,k):
+    i=j=0
+    m={}
+    mx=0
+    # x=Counter(arr)
+    # if len(x) <k: return -1  #for case like 'aavvcc'      k=4
+    while j<len(arr):
+        if arr[j] in m:
+            m[arr[j]] +=1
+
+        else: m[arr[j]] =1
+
+        if len(m) < k:
+            j+=1
+
+        elif len(m) ==k:
+            mx=max(mx, j-i+1)
+            j+=1
+
+        else:
+            while len(m) > k:
+                it=arr[i]
+                m[it] -=1 #m[arr[i]] gives string index out of range ! although we did same work here ! 
+                i+=1
+                if m[it]==0:
+                    m.pop(it)
+            j+=1
+
+    return mx
+
+print(sol('aabbcc',2))
